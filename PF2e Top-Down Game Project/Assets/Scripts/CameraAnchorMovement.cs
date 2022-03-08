6417150b1c426008cf81b1b62aa520e0dd223668
QwertTheWert <fredrickwill2005@gmail.com>
2022-03-08 11:28:48 +0700

    Implemented scrolling

--- PF2e Top-Down Game Project/Assets/Scripts/CameraAnchorMovement.cs
+++ PF2e Top-Down Game Project/Assets/Scripts/CameraAnchorMovement.cs
@@ -3,21 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraAnchorMovement : MonoBehaviour {
-	public float moveSpeed = 5f;
-	public Rigidbody rb;
+	[SerializeField] private float moveSpeed = 5f;
+	[SerializeField] private float edgeSize = 100f;
+	[SerializeField] private bool enableEdgeScroll = true;
+	[SerializeField] private Rigidbody rb;
 
 	Vector3 movement;
 
-	// Update is called once per frame
 	void Update() {
 		// Input
 		movement.x = Input.GetAxisRaw("Horizontal");
 		movement.z = Input.GetAxisRaw("Vertical");
+
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			enableEdgeScroll = !enableEdgeScroll;
+		}
 		
+		if (enableEdgeScroll) {
+			EdgeScroll();
+		}
 	}
 
 	void FixedUpdate() {
 		// Movement
 		rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+	}
+
+	void EdgeScroll() {
+		// Edge Right
+		if (Input.mousePosition.x > Screen.width - edgeSize) {
+			movement.x = 1;
+		}
+		// Edge Left
+		if (Input.mousePosition.x < edgeSize) {
+			movement.x = -1;
+		}
+		// Edge Up
+		if (Input.mousePosition.y > Screen.height - edgeSize) {
+			movement.z = 1;
+		}
+		// Edge Down
+		if (Input.mousePosition.y < edgeSize) {
+			movement.z = -1;
+		}
 	}
 }
--- PF2e Top-Down Game Project/Assets/Scripts/GameHandler.cs
+++ PF2e Top-Down Game Project/Assets/Scripts/GameHandler.cs
@@ -7,39 +7,33 @@ public class GameHandler : MonoBehaviour
 	[SerializeField] private CameraFollow cameraFollow;
 	[SerializeField] private Transform playerTransform;
 	// [SerializeField] private int targetFrameRate = 60;
-
-	[SerializeField] private float zoom = 5;
-	private bool zoomIn;
-	private bool zoomOut;
-
-
+	[SerializeField] private float zoom = 5f;
+	[SerializeField] private float zoomChangeAmount = 0.1f;
+	[SerializeField] private float minZoom = 5f;
+	[SerializeField] private float maxZoom = 40f;
 
 	// Start is called before the first frame update
-	void Start()
-	{
+	void Start() {
 		// cameraFollow.set
 		cameraFollow.Setup(() => playerTransform.position, () => zoom);
 	}
 
-	void Update() {
-		// Zoom Key Input
-		zoomIn = Input.GetKey("-");
-		zoomOut = Input.GetKey("=");
-	}
-
 	void FixedUpdate() {
-		// Update Zoom
-		if (zoomIn && !zoomOut) ZoomIn();
-		if (!zoomIn && zoomOut) ZoomOut();
-	}
+		// Zoom Key Input
+		if (Input.GetKey(KeyCode.KeypadPlus)) {
+			zoom -= zoomChangeAmount * Time.fixedDeltaTime;
+		}
+		if (Input.GetKey(KeyCode.KeypadMinus)) {
+			zoom += zoomChangeAmount * Time.fixedDeltaTime;
+		}
 
-	private void ZoomIn() {
-		zoom -= 1f;
-		if (zoom < 5f) zoom = 5f;
-	}
+		if (Input.mouseScrollDelta.y > 0) {
+			zoom -= zoomChangeAmount * Time.fixedDeltaTime * 100;
+		}
+		if (Input.mouseScrollDelta.y < 0) {
+			zoom += zoomChangeAmount * Time.fixedDeltaTime * 100;
+		}
 
-	private void ZoomOut() {
-		zoom += 1f;
-		if (zoom > 40f) zoom = 40f;
-	}
+		zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+	}	
 }
